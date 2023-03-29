b24ddfe6f30e20c97bace133006945fbc5aae8a5
Amr Tamer <Amrtamer23mj@gmail.com>
2023-03-29 04:32:42 +0200

    updating the breaking news list view

--- lib/features/home/view/widgets/breaking_list_view.dart
+++ lib/features/home/view/widgets/breaking_list_view.dart
@@ -1,34 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
-
+import 'package:carousel_slider/carousel_slider.dart';
 import 'breaking_item.dart';
+import 'package:carousel_slider/carousel_options.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 
-class BreakingListView extends StatelessWidget {
+class BreakingListView extends StatefulWidget {
   const BreakingListView({super.key});
 
   @override
+  State<BreakingListView> createState() => _BreakingListViewState();
+}
+
+class _BreakingListViewState extends State<BreakingListView> {
+  int currentIndex = 0;
+  @override
   Widget build(BuildContext context) {
-    final PageController pageController = PageController();
     return Column(
       children: [
         Column(
           children: [
             SizedBox(
-              height: 250,
-              child: ListView.builder(
-                padding: EdgeInsets.zero,
-                physics: const BouncingScrollPhysics(),
-                scrollDirection: Axis.horizontal,
-                controller: pageController,
+              child: CarouselSlider.builder(
                 itemCount: 10,
-                itemBuilder: (context, index) {
+                itemBuilder: (context, index, realIndex) {
                   return const BreakingItem();
                 },
+                options: CarouselOptions(
+                  viewportFraction: 0.869,
+                  autoPlay: false,
+                  height: 230,
+                  enableInfiniteScroll: false,
+                  enlargeCenterPage: true,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      currentIndex = index;
+                    });
+                  },
+                ),
               ),
             ),
             const SizedBox(
               height: 10,
             ),
+            DotsIndicator(
+              dotsCount: 10,
+              position: currentIndex.toDouble(),
+              decorator: DotsDecorator(
+                size: const Size.square(11.0),
+                activeSize: const Size(20.0, 9.0),
+                color: Colors.black12,
+                activeShape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0)),
+              ),
+            ),
+            /*
             SmoothPageIndicator(
               controller: pageController,
               count: 10,
@@ -38,6 +64,7 @@ class BreakingListView extends StatelessWidget {
                   dotColor: Color.fromARGB(39, 158, 158, 158),
                   activeDotColor: Colors.blue),
             )
+            */
           ],
         ),
       ],
